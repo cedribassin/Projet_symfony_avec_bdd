aeea77b45764e61a145222d9067523e684eced3c
Cédric R <cedric.ribassin@gmail.com>
2020-10-20 13:18:03 -0400

    Ajout et utilisation de la table (classe) Personne  qui est liée à la table Animal via la table Dispose par une relation 1-n  1-n en merise, et (Animal)1-1, 1-n (Dispose) 1-n, 1-1(Personne) dans Symfony, car nous avons ajouté une propriété nb dans l'association entre Animal et Personne

--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -5,6 +5,8 @@ namespace App\DataFixtures;
 use App\Entity\Animal;
 use App\Entity\Famille;
 use App\Entity\Continent;
+use App\Entity\Dispose;
+use App\Entity\Personne;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -12,6 +14,18 @@ class AnimalFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $p1=new Personne();
+        $p1->setNom("Cedric");
+        $manager->persist($p1);
+
+        $p2=new Personne();
+        $p2->setNom("Lea");
+        $manager->persist($p2);
+
+        $p3=new Personne();
+        $p3->setNom("Leeroy");
+        $manager->persist($p3);
+
         $continent1 = new Continent();
         $continent1->setLibelle("Europe");
         $manager->persist($continent1);
@@ -121,8 +135,43 @@ class AnimalFixtures extends Fixture
 
         $manager->persist($a5);
 
-        // $product = new Product();
-        // $manager->persist($product);
+        //On crée un objet dispose qui indique que:
+        //La personne $p1 dispose de 3 animaux $a5
+        $d1 = new Dispose();
+        $d1->setPersonne($p1)
+        ->setAnimal($a5)
+        ->setNb(3);
+        $manager->persist($d1);
+
+        $d2 = new Dispose();
+        $d2->setPersonne($p2)
+        ->setAnimal($a1)
+        ->setNb(10);
+        $manager->persist($d2);
+
+        $d3 = new Dispose();
+        $d3->setPersonne($p3)
+        ->setAnimal($a3)
+        ->setNb(15);
+        $manager->persist($d3);
+
+        $d4 = new Dispose();
+        $d4->setPersonne($p1)
+        ->setAnimal($a3)
+        ->setNb(5);
+        $manager->persist($d4);
+
+        $d5 = new Dispose();
+        $d5->setPersonne($p2)
+        ->setAnimal($a2)
+        ->setNb(5);
+        $manager->persist($d5);
+
+        $d6 = new Dispose();
+        $d6->setPersonne($p3)
+        ->setAnimal($a4)
+        ->setNb(6);
+        $manager->persist($d6);
 
         $manager->flush();
     }
